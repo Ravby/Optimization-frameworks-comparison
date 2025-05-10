f6868c66290c8d74fc9e87ae084fdc198ef0cc30
SmidMarko <marko.smid10@gmail.com>
2025-05-10 09:38:50 +0200

    Pagmo2 problems update, new algorithm results.

--- Frameworks/pagmo2/tests/schwefel.cpp
+++ Frameworks/pagmo2/tests/schwefel.cpp
@@ -26,42 +26,30 @@
 #include <pagmo/problems/camel_back.hpp>
 #include <pagmo/problems/branin.hpp>
 #include <pagmo/problems/goldstein_price.hpp>
-#include <pagmo/problems/hartman.hpp>
+#include <pagmo/problems/hartman.hpp> 
+#include <pagmo/algorithms/cmaes.hpp>
 
 using namespace pagmo;
 
 int main()
 {
     // 1 - Instantiate a pagmo problem constructing it from a UDP
-    problem probSphere{sphere(30)};
-    problem probSumOfSquares{sumOfSquares(30)};
-    problem proSschwefel{schwefel(30)};
-    problem probRastrigin{rastrigin(30)};
-    problem probAckley{ackley(30)};
-    problem probGriewank{griewank(30)};
-    problem probRosenbrock{rosenbrock(30)};
+    problem probSphere{sphere(60)};
+    problem probSumOfSquares{sumOfSquares(60)};
+    problem proSschwefel{schwefel(60)};
+    problem probRastrigin{rastrigin(60)};
+    problem probAckley{ackley(60)};
+    problem probGriewank{griewank(60)};
+    problem probRosenbrock{rosenbrock(60)};
     problem probShekelFoxHoles{shekelsFoxHoles(2)};
     problem probCamelBack{camelBack(2)};
     problem probBranin{branin(2)};
     problem probFoldsteinPrice{goldsteinPrice(2)};
     problem probHartman{hartman(3)};
 
-    /* std::cout << "Sphere: " << probSphere.fitness(probSphere.best_known())[0] << '\n';
-    std::cout << "Sum fo Squares: " << probSumOfSquares.fitness(probSumOfSquares.best_known())[0] << '\n';
-    std::cout << "Schwefel: " << proSschwefel.fitness(proSschwefel.best_known())[0] << '\n';
-    std::cout << "Rastrigin: " << probRastrigin.fitness(probRastrigin.best_known())[0] << '\n';
-    std::cout << "Ackley: " << probAckley.fitness(probAckley.best_known())[0] << '\n';
-    std::cout << "Griewank: " << probGriewank.fitness(probGriewank.best_known())[0] << '\n';
-    std::cout << "Rosenbrook: " << probRosenbrock.fitness(probRosenbrock.best_known())[0] << '\n';
-    std::cout << "Fox holes: " << probShekelFoxHoles.fitness(probShekelFoxHoles.best_known())[0] << '\n';
-    std::cout << "Camel back: " << probCamelBack.fitness(probCamelBack.best_known())[0] << '\n';
-    std::cout << "Branin: " << probBranin.fitness(probBranin.best_known())[0] << '\n';
-    std::cout << "Goldstein price: " << probFoldsteinPrice.fitness(probFoldsteinPrice.best_known())[0] << '\n';
-    std::cout << "Hartman: " << probHartman.fitness(probHartman.best_known())[0] << '\n';*/
-
-
     // Algorithm initialization
-    algorithm algABC{bee_colony(60, 100)}; // bee_colony(unsigned gen = 1u, unsigned limit = 20u, unsigned seed = pagmo::random_device::next());
+    algorithm algABC{bee_colony(
+        60, 100)}; // bee_colony(unsigned gen = 1u, unsigned limit = 20u, unsigned seed = pagmo::random_device::next());
     //algABC.set_verbosity(1);
     algorithm algPSO{pso(499, 0.7, 2., 2.)}; // pso(unsigned gen = 1u, double omega = 0.7298, double eta1 = 2.05, double eta2 = 2.05, double max_vel = 0.5, unsigned variant = 5u, unsigned neighb_type = 2u, unsigned neighb_param = 4u, bool memory = false, unsigned seed = pagmo::random_device::next());
     //algPSO.set_verbosity(1);
@@ -71,22 +59,26 @@ int main()
     //algGWO.set_verbosity(1);
     algorithm algSGA{sga(149, 0.95, 20., 0.025, 20, 2, "sbx", "polynomial")}; // sga(unsigned gen = 1u, double cr = .90, double eta_c = 1., double m = 0.02, double param_m = 1.,unsigned param_s = 2u, std::string crossover = "exponential", std::string mutation = "polynomial",std::string selection = "tournament", unsigned seed = pagmo::random_device::next());
     //algSGA.set_verbosity(1);
+    algorithm algCMAES{cmaes()};
 
-    std::vector<algorithm> algorithms{algABC, algPSO, algDE, algGWO, algSGA};
-    //std::vector<algorithm> algorithms{algSGA};
+    std::vector<algorithm> algorithms{algABC, algPSO, algDE, algGWO, algSGA, algCMAES};
+    //std::vector<algorithm> algorithms{algABC, algCMAES};
     
-    //std::vector<problem> problems {probSphere,     probSumOfSquares,   proSschwefel,  probRastrigin, probAckley,         probGriewank,     probRosenbrock,     probShekelFoxHoles,probCamelBack, probBranin,       probFoldsteinPrice, probHartman    };
-    std::vector<problem> problems {probSphere,     probSumOfSquares,   proSschwefel,  probRastrigin, probAckley,         probGriewank,     probRosenbrock};
+    std::vector<problem> problems {probSphere,     probSumOfSquares,   proSschwefel,  probRastrigin, probAckley,         probGriewank,     probRosenbrock,     probShekelFoxHoles,probCamelBack, probBranin,       probFoldsteinPrice, probHartman    };
+    //std::vector<problem> problems {probSphere,     probSumOfSquares,   proSschwefel,  probRastrigin, probAckley,         probGriewank,     probRosenbrock};
 
     std::string frameworkName = "pagmo2";
-    std::vector<std::string> algorithmNames{"ABC", "PSO", "DE", "GWO", "GA"}; 
+
+    std::vector<std::string> algorithmNames{"ABC", "PSO", "DE", "GWO", "GA", "CMAES"};  
 
     std::vector<std::string> problemNames{
         "Sphere", "SumOfSquares", "Schwefel", "Rastrigin", "Ackley", "Griewank",
         "Rosenbrock", "ShekelsFoxholes", "SixHumpCamelBack", "Branin", "GoldsteinPrice", "Hartman",
     }; 
     std::vector<int> dims{60, 60, 60, 60, 60, 60, 60, 2, 2, 2, 2, 3}; 
-    std::vector<int> numOfIndividuals{ 125, 30, 50, 30, 100};
+    
+    std::vector<int> numOfIndividuals{ 125, 30, 50, 30, 100, 30};
+    //std::vector<int> numOfIndividuals{125, 30};
 
 
     unsigned numberOfRepeates = 100u;
